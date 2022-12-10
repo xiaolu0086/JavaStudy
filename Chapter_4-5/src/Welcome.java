8aa62a53fe7c69815d873ac5899cd39f04660cc5
xiaolu0086 <radiamiiii@hotmail.com>
2022-12-11 04:23:50 +0800

    add Chapter 4&5 files

---
+++ Chapter_4-5/src/Welcome.java
@@ -0,0 +1,17 @@
+public class Welcome{
+	public static void main(String[] args){  //args arguments
+		System.out.println("Hello World!");
+	}
+}
+
+class Car{
+	
+}
+
+class Man{
+	
+}
+
+class Computer{
+	
+}
